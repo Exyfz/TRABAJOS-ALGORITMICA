78f3e4f9b3c4c2db383b74c8c0c6e962b265e30d
Emiliano Meza Cervantes <emilianomezacervantesx@gmail.com>
2024-09-22 17:04:01 -0700

    Add files via upload

---
+++ 6-EDAD AGIL IOSTREAM/EDAD AGIL IOSTREAM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string usuario;
+   int edad;
+
+   cout << "Introduce tu usuario: ";
+   cin >> usuario;
+   cout << "introduzca su edad: ";
+   cin >> edad;
+
+   cout << "usuario: "<< usuario << endl;
+   cout << "Edad: " << edad << " anios" << endl;
+   return 0;
+}
